2bc5ed6e31ce80075684d4df0b0bf5d9f30f7d61
LAWAL ABD'KEHINDE <32939546+kennie-larkson@users.noreply.github.com>
2020-07-21 14:12:14 +0100

    refactored Card component and created Details component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,14 +7,12 @@ function Card(props) {
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        {/* <img className="circle-img" src={props.img} alt="avatar_img" /> */}
         <Avatar imgSrc={props.img} />
       </div>
-      {/* <div className="bottom">
-        <p className="info">{props.tel}</p>
-        <p className="info">{props.email}</p>
-      </div> */}
-      <Details tel={props.tel} email={props.email} />
+      <div className="bottom">
+      <Details detailInfo={props.tel} />
+      <Details detailInfo={props.email} />
+      </div>
     </div>
   );
 }
