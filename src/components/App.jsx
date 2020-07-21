d13177b8e36dbddc33f2394cbd31b22205291b77
LAWAL ABD'KEHINDE <32939546+kennie-larkson@users.noreply.github.com>
2020-07-21 14:55:47 +0100

    accessed a special props in Card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,31 +3,26 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      id={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <Avatar imgSrc="https://avatars0.githubusercontent.com/u/32939546?s=460&u=ad7d7482303ac0be157607acebdae8e03fa912c4&v=4" />
       <h1 className="heading">My Contacts</h1>
 
-      <Card
-        name={contacts[0].name}
-        // img={contacts[0].imgURL
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(createCard)}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,12 +6,13 @@ function Card(props) {
   return (
     <div className="card">
       <div className="top">
+        <p className="info">{props.id}</p>
         <h2 className="name">{props.name}</h2>
         <Avatar imgSrc={props.img} />
       </div>
       <div className="bottom">
-      <Details detailInfo={props.tel} />
-      <Details detailInfo={props.email} />
+        <Details detailInfo={props.tel} />
+        <Details detailInfo={props.email} />
       </div>
     </div>
   );
